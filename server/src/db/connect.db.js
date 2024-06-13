4a3779b3181107a45e7574ced28d0a390e4fd85d
mces58 <ceser446@gmail.com>
2024-06-13 16:22:49 +0300

    chore(db-connection): optimize mongo-db connection handling

--- server/src/db/connect.db.js
+++ server/src/db/connect.db.js
@@ -3,13 +3,39 @@ import mongoose from 'mongoose';
 import dotEnvConfig from 'src/configs/dotEnv.config';
 import logger from 'src/utils/logger.util';
 
-const connectToMongoDB = async () => {
-  try {
-    await mongoose.connect(dotEnvConfig.DATABASE_URI);
-    logger.info('Connected to MongoDB');
-  } catch (error) {
-    logger.error(error);
-  }
+const connectToMongoDB = () => {
+  const reconnectTimeout = 5000;
+
+  mongoose.Promise = global.Promise;
+  const db = mongoose.connection;
+
+  const connect = async () => {
+    mongoose.connect(dotEnvConfig.DATABASE_URI);
+  };
+
+  db.on('connected', () => {
+    logger.info('Connected to MongoDB!');
+  });
+
+  db.on('error', (error) => {
+    logger.error(`MongoDB connection error: ${error}`);
+    mongoose.disconnect();
+  });
+
+  db.once('open', () => {
+    logger.info('MongoDB connection opened!');
+  });
+
+  db.on('reconnected', () => {
+    logger.info('MongoDB reconnected!');
+  });
+
+  db.on('disconnected', () => {
+    logger.error(`MongoDB disconnected! Reconnecting in ${reconnectTimeout / 1000}s...`);
+    setTimeout(connect, reconnectTimeout);
+  });
+
+  connect();
 };
 
 export default connectToMongoDB;
