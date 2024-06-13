b001bfcc551bfbae467907d4d72672dd230e6fb1
mces58 <ceser446@gmail.com>
2024-06-13 14:36:48 +0300

    fix(user-controller-route): fixed user controller and route, also unnecessary code have been cleaned

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,203 +1,38 @@
-import Conversation from 'src/models/conversation.model';
 import User from 'src/models/user.model';
+import handleErrors from 'src/utils/error.util';
 
-const getUsers = async (req, res) => {
+export const getUsers = async (req, res) => {
   try {
-    const loggedInUser = req.user._id;
+    const loggedInUser = req.user.id;
+    const users = await User.find({ _id: { $ne: loggedInUser } });
 
-    const users = await User.find({ _id: { $ne: loggedInUser } }).select('-password');
-
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const getLastMessagesWithOtherUsers = async (req, res) => {
-  try {
-    const { userId } = req.body;
-
-    const conversations = await Conversation.find({
-      participants: userId,
-      deletedBy: { $ne: userId },
-    })
-      .sort({ updatedAt: -1 })
-      .populate({
-        path: 'messages',
-        options: { sort: { createdAt: -1 }, limit: 1 },
-      })
-      .populate('participants', '_id fullName profilePicture');
-
-    const lastMessages = conversations.map((conversation) => {
-      const otherParticipant = conversation.participants.find(
-        (participant) => participant._id.toString() !== userId.toString()
-      );
-      return {
-        _id: otherParticipant._id,
-        fullName: otherParticipant.fullName,
-        profilePicture: otherParticipant.profilePicture,
-        lastMessage: conversation.messages.length > 0 ? conversation.messages[0] : null,
-      };
+    res.status(200).json({
+      success: true,
+      data: users,
     });
-
-    res.status(200).json(lastMessages);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    handleErrors(res, error);
   }
 };
 
-const getUserById = async (req, res) => {
-  try {
-    const { userId } = req.params;
-
-    const user = await User.findById(userId).select('-password');
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const sendFriendRequest = async (req, res) => {
-  const { currentUserId, selectedUserId } = req.body;
+export const getUserById = async (req, res) => {
+  const { userId } = req.params;
 
   try {
-    await User.findByIdAndUpdate(selectedUserId, {
-      $push: { freindRequests: currentUserId },
-    });
-
-    await User.findByIdAndUpdate(currentUserId, {
-      $push: { sentFriendRequests: selectedUserId },
-    });
-
-    res.sendStatus(200);
-  } catch (error) {
-    res.sendStatus(500);
-  }
-};
-
-const getByIdFriendRequest = async (req, res) => {
-  try {
-    const { userId } = req.params;
-
-    const user = await User.findById(userId)
-      .populate('freindRequests', 'userName profilePicture fullName')
-      .lean();
-
-    const { freindRequests } = user;
-
-    res.json(freindRequests);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-};
-
-const sendAcceptFriendRequest = async (req, res) => {
-  try {
-    const { senderId, recepientId } = req.body;
-
-    const sender = await User.findById(senderId);
-    const recepient = await User.findById(recepientId);
-
-    sender.friends.push(recepientId);
-    recepient.friends.push(senderId);
-
-    recepient.freindRequests = recepient.freindRequests.filter(
-      (request) => request.toString() !== senderId.toString()
-    );
-
-    sender.sentFriendRequests = sender.sentFriendRequests.filter(
-      (request) => request.toString() !== recepientId.toString()
-    );
-
-    await sender.save();
-    await recepient.save();
-
-    await Conversation.updateMany(
-      {
-        $or: [{ userIds: [senderId, recepientId] }, { userIds: [recepientId, senderId] }],
-      },
-      {
-        $pull: { deletedBy: { $in: [senderId, recepientId] } },
-      }
-    );
-
-    res.status(200).json({ message: 'Friend Request accepted successfully' });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-};
-
-const getByIdAcceptFriendRequest = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const user = await User.findById(userId).populate(
-      'friends',
-      'userName profilePicture fullName'
-    );
-    const acceptedFriends = user.friends;
-    res.json(acceptedFriends);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-const getNonFriends = async (req, res) => {
-  try {
-    const loggedInUserId = req.params.userId;
-    const loggedInUser = await User.findById(loggedInUserId).populate('friends', '_id');
-    const friendIds = loggedInUser.friends.map((friend) => friend._id.toString());
-
-    const nonFriends = await User.find({
-      _id: { $ne: loggedInUserId, $nin: friendIds },
-    }).select('-password');
-
-    res.status(200).json(nonFriends);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-const removeFriend = async (req, res) => {
-  try {
-    const { userId, friendId } = req.params;
-
     const user = await User.findById(userId);
-    const friend = await User.findById(friendId);
 
-    if (!user || !friend) {
-      return res.status(404).json({ message: 'User not found' });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+      });
     }
 
-    user.friends = user.friends.filter((id) => id.toString() !== friendId.toString());
-    friend.friends = friend.friends.filter((id) => id.toString() !== userId.toString());
-
-    await Conversation.updateMany(
-      { participants: { $all: [userId, friendId] } },
-      { $addToSet: { deletedBy: userId } }
-    );
-
-    await user.save();
-    await friend.save();
-
-    res.status(200).json({ message: 'Friend removed successfully' });
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    handleErrors(res, error);
   }
-};
-
-export {
-  getUsers,
-  getLastMessagesWithOtherUsers,
-  getUserById,
-  sendFriendRequest,
-  getByIdFriendRequest,
-  sendAcceptFriendRequest,
-  getByIdAcceptFriendRequest,
-  getNonFriends,
-  removeFriend,
 };
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,36 +1,18 @@
 import { Router } from 'express';
 
-import {
-  getByIdAcceptFriendRequest,
-  getByIdFriendRequest,
-  getLastMessagesWithOtherUsers,
-  getNonFriends,
-  getUserById,
-  getUsers,
-  removeFriend,
-  sendAcceptFriendRequest,
-  sendFriendRequest,
-} from 'src/controllers/user.controller';
+import { getUserById, getUsers } from 'src/controllers/user.controller';
 import authentication from 'src/middlewares/authentication.middleware';
 
 const router = Router();
 
-router.get('/', authentication, getUsers);
+// @route   GET /api/users
+// @desc    Get all users
+// @access  Private
+router.route('/').get(authentication, getUsers);
 
-router.post('/last-messages/', getLastMessagesWithOtherUsers);
-
-router.get('/:userId', authentication, getUserById);
-
-router.post('/friend-request', authentication, sendFriendRequest);
-
-router.get('/friend-request/:userId', authentication, getByIdFriendRequest);
-
-router.post('/friend-request/accept', authentication, sendAcceptFriendRequest);
-
-router.get('/accepted-friends/:userId', authentication, getByIdAcceptFriendRequest);
-
-router.get('/non-friends/:userId', authentication, getNonFriends);
-
-router.delete('/remove-friend/:userId/:friendId', authentication, removeFriend);
+// @route   GET /api/users/:userId
+// @desc    Get user by ID
+// @access  Private
+router.route('/:userId').get(authentication, getUserById);
 
 export default router;
